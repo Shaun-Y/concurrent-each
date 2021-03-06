c95bd57eafeb40c94f8999323a1f73ae4f289a2b
xdact <xdact@outlook.com>
2021-03-06 14:18:46 +0000

    Add forEach and reduce operations

--- src/index.js
+++ src/index.js
@@ -32,6 +32,75 @@ const map = (arr, mapper, batch) => {
   });
 };
 
+/**
+ * @param {Array} arr
+ * @param {Function} callback
+ * @param {Number} [batch] Batch size
+ */
+const forEach = (arr, callback, batch) => {
+  // If no batch set then default to 1000 operations
+  if (!batch) {
+    batch = Math.ceil(arr.length / 1000);
+  }
+
+  return new Promise((resolve) => {
+    let i = 0;
+
+    const execute = () => {
+      if (i < arr.length - batch) {
+        setImmediate(execute); // Schedule next batch onto task queue
+      }
+
+      arr.slice(i, i + batch).forEach(callback);
+
+      i += batch;
+
+      if (i >= arr.length) {
+        resolve();
+      }
+    };
+
+    setImmediate(execute); // First batch
+  });
+};
+
+/**
+ * @param {*} arr
+ * @param {*} reducer
+ * @param {*} initialValue
+ * @param {Number} [batch] Batch size
+ *
+ */
+const reduce = (arr, reducer, initialValue, batch) => {
+  // If no batch set then default to 1000 operations
+  if (!batch) {
+    batch = Math.ceil(arr.length / 1000);
+  }
+
+  return new Promise((resolve) => {
+    let i = 0;
+    let accumulator = initialValue;
+    // let lastValue = initialValue;
+
+    const execute = () => {
+      if (i < arr.length - batch) {
+        setImmediate(execute); // Schedule next batch onto task queue
+      }
+
+      accumulator = arr.slice(i, i + batch).reduce(reducer, accumulator);
+      i += batch;
+
+      if (i >= arr.length) {
+        resolve(accumulator);
+      }
+    };
+
+    setImmediate(execute); // First batch
+  });
+};
+
 module.exports = {
   map,
+  forEach,
+  reduce,
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,32 +1,60 @@
 const ceach = require('./index');
 
 const testData = new Array(1e4).fill('a');
+const testDataNumeric = new Array(1e4).fill(1);
 
 describe('concurrent-each', () => {
   it('map 1 batch', async () => {
-    let expected = testData.map((d) => d.toUpperCase());
-    let actual = await ceach.map(testData, (data) => data.toUpperCase(), 1);
+    const expected = testData.map((d) => d.toUpperCase());
+    const actual = await ceach.map(testData, (data) => data.toUpperCase(), 1);
 
     expect(actual).toEqual(expected);
   });
 
   it('map 10 batches', async () => {
-    let expected = testData.map((d) => d.toUpperCase());
-    let actual = await ceach.map(testData, (data) => data.toUpperCase(), 10);
+    const expected = testData.map((d) => d.toUpperCase());
+    const actual = await ceach.map(testData, (data) => data.toUpperCase(), 10);
 
     expect(actual).toEqual(expected);
   });
 
   it('map 100 batches', async () => {
-    let expected = testData.map((d) => d.toUpperCase());
-    let actual = await ceach.map(testData, (data) => data.toUpperCase(), 100);
+    const expected = testData.map((d) => d.toUpperCase());
+    const actual = await ceach.map(testData, (data) => data.toUpperCase(), 100);
 
     expect(actual).toEqual(expected);
   });
 
   it('map 1000 batches', async () => {
-    let expected = testData.map((d) => d.toUpperCase());
-    let actual = await ceach.map(testData, (data) => data.toUpperCase(), 1000);
+    const expected = testData.map((d) => d.toUpperCase());
+    const actual = await ceach.map(testData, (data) => data.toUpperCase(), 1000);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('forEach 10 batch', async () => {
+    const expectedOne = testData.map((d) => d.toUpperCase());
+    const expectedTwo = testData.map((d) => d.toUpperCase());
+
+    const actualOne = [];
+    const actualTwo = [];
+
+    await ceach.forEach(
+      testData,
+      (data) => {
+        actualOne.push(data.toUpperCase());
+        actualTwo.push(data.toUpperCase());
+      },
+      10
+    );
+
+    expect(actualOne).toEqual(expectedOne);
+    expect(actualTwo).toEqual(expectedTwo);
+  });
+
+  it('reduce 10 batch', async () => {
+    const expected = testDataNumeric.reduce((acc, curr) => (acc += curr));
+    const actual = await ceach.reduce(testDataNumeric, (acc, curr) => (acc += curr), 0, 10);
 
     expect(actual).toEqual(expected);
   });
